f74461107c17301f013b4696ed69bfeea0db3ac7
Pillai <223027092@ge.com>
2022-09-06 12:20:52 +0530

    added changes

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -17,19 +17,61 @@ export default async function (event, context, logger) {
     logger.info(`Invoking parse below-------->>`);
     const payload = event.data;
     var oppID = '' ;
-    try{
+    
     for(let i = 0; i < payload.length; i++) {
         oppID = oppID+"'"+payload[i].Id +"',";
     }
     oppID = "("+oppID.slice(0, -1)+")";
     logger.info(`Invoking oppID ----->> ${oppID}`);
     logger.info(`Invoking payload ----->> ${payload[0].Id}`);
-    }catch(e){
-        logger.info(`exception===>>${e}`);
-    }
+   
     const results = await context.org.dataApi.query(`SELECT Opportunity_Name__c ,Id,Name FROM Revenue__c WHERE Opportunity_Name__c IN ${oppID}`);
 
     logger.info(JSON.stringify(results));
+    var revDelete = []; 
+    const uow = context.org.dataApi.newUnitOfWork();
+    for(let i = 0; i < results.length; i++){
+        const revDML = uow.registerDelete({
+        type: "Revenue__c",
+        id:results.Id
+        });
+        revDelete.push(revDML);
+    }
+    const oliQuery = await context.org.dataApi.query(`Select opportunityId,Business__c,Modality_Offering__c,PricebookEntryId,PricebookEntry.Product2Id, TotalPrice, UnitPrice,Modality__c,Product_Name__c, ListPrice,Domain_Name__c ,Business_Category__c From OpportunityLineItem where opportunityId IN ${oppID} AND TotalPrice!=0`);
+    const oliOptyMap = new Map();
+    for(let i = 0; i < oliQuery.length; i++){
+        oliOptyMap.set(oliQuery[i].opportunityId,oliQuery[i]);
+    }
+    /*for(let i = 0; i < payload.length; i++){ 
+        if(payload[i].Amount!=null && payload[i].Estimated_Sales_Date__c!=null && payload[i].Term_New__c!=null && payload[i].Term_New__c!=0){
+            differenceAmount =0;
+                            //getting no of days for the first term , it should be always minus from the 5th of next month.its just for the showing purpose 
+                            //Amount would be always calculated by considering 30 days in month and every month starts from 1 
+                            
+                            Decimal dayInt = (date.daysInMonth((ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c).year()),(ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c)).month())-(ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c).day()))+5;
+                              system.debug('***** dayInt -:'+dayInt);              
+                            //this is for the generating revenue for the first month
+                            Decimal dayFirstRecInt =0;
+                            dayFirstRecInt=(date.daysInMonth((ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c).year()),(ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c)).month())) - (ConvertDateToDateTime( payload[i].Estimated_Sales_Date__c).day())+1;
+                             system.debug('***** dayFirstRecInt -:'+dayFirstRecInt);                
+                            //getting total no of days for the opportunity to get the amount for specific month
+                            Decimal noDaysInt = 0;
+                            
+        }
+    }*/
+    try {
+        // Commit the Unit of Work with all the previous registered operations
+        const response = await context.org.dataApi.commitUnitOfWork(uow);
+        
+        return response;
+      } catch (err) {
+        const errorMessage = `Failed to insert record. Root Cause : ${err.message}`;
+        logger.error(errorMessage);
+        throw new Error(errorMessage);
+      }
+    //var retResponse = "Deleted revenue record";
 
-    return results;
+    
+
+    return oliOptyMap;
 }
