e1962e5c6665a6f710d02cd630f86edec66f1ece
Pillai <223027092@ge.com>
2022-09-12 15:08:49 +0530

    addded changes

--- functions/myfunction/index.js
+++ functions/myfunction/index.js
@@ -1,43 +1,52 @@
 /**
  * Describe Myfunction here.
  *
- * The exported method is the entry point for your code when the function is invoked. 
+ * The exported method is the entry point for your code when the function is invoked.
  *
- * Following parameters are pre-configured and provided to your function on execution: 
- * @param event: represents the data associated with the occurrence of an event, and  
+ * Following parameters are pre-configured and provided to your function on execution:
+ * @param event: represents the data associated with the occurrence of an event, and
  *                 supporting metadata about the source of that occurrence.
  * @param context: represents the connection to Functions and your Salesforce org.
  * @param logger: logging handler used to capture application logs and trace specifically
  *                 to a given execution of a function.
  */
- 
-export default async function (event, context, logger) {
-    logger.info(`Invoking Myfunction with payload `);
-    logger.info(`Invoking Myfunction with payload ${JSON.stringify(event.data || {})}`);
-    logger.info(`Invoking parse below-------->>`);
-    const payload = event.data;
-    var oppID = '' ;
-    
-    for(let i = 0; i < payload.length; i++) {
-        oppID = oppID+"'"+payload[i].Id +"',";
-    }
-    oppID = "("+oppID.slice(0, -1)+")";
-    logger.info(`Invoking oppID ----->> ${oppID}`);
-    logger.info(`Invoking payload ----->> ${payload[0].Id}`);
-   
-    const results = await context.org.dataApi.query(`SELECT Opportunity_Name__c ,Id,Name FROM Revenue__c WHERE Opportunity_Name__c IN ${oppID}`);
 
-    logger.info(JSON.stringify(results));
-    try {
-    var revDelete = []; 
+export default async function (event, context, logger) {
+  logger.info(`Invoking Myfunction with payload `);
+  logger.info(
+    `Invoking Myfunction with payload ${JSON.stringify(event.data || {})}`
+  );
+  logger.info(`Invoking parse below-------->>`);
+  const payload = event.data;
+  var oppID = "";
+
+  for (let i = 0; i < payload.length; i++) {
+    oppID = oppID + "'" + payload[i].Id + "',";
+  }
+  oppID = "(" + oppID.slice(0, -1) + ")";
+  logger.info(`Invoking oppID ----->> ${oppID}`);
+  logger.info(`Invoking payload ----->> ${payload[0].Id}`);
+  const results = await context.org.dataApi.query(
+    `SELECT Id,Name FROM Account`
+  );
+
+  const results = await context.org.dataApi.query(
+    `SELECT Opportunity_Name__c ,Id,Name FROM Revenue__c WHERE Opportunity_Name__c IN ${oppID}`
+  );
+
+  logger.info(JSON.stringify(results));
+  try {
+    var revDelete = [];
     const uow = context.org.dataApi.newUnitOfWork();
-    for(let i = 0; i < results.records.length; i++){
-      logger.info(`revDelete inside forloop ----->> ${results.records[i].fields.Id}`);
-        const revDML = uow.registerDelete({
+    for (let i = 0; i < results.records.length; i++) {
+      logger.info(
+        `revDelete inside forloop ----->> ${results.records[i].fields.Id}`
+      );
+      const revDML = uow.registerDelete({
         type: "Revenue__c",
-        id:results.records[i].fields.Id
-        });
-        revDelete.push(revDML);
+        id: results.records[i].fields.Id
+      });
+      revDelete.push(revDML);
     }
     logger.info(`revDelete ----->> ${revDelete}`);
     /*const oliQuery = await context.org.dataApi.query(`Select opportunityId,Business__c,Modality_Offering__c,PricebookEntryId,PricebookEntry.Product2Id, TotalPrice, UnitPrice,Modality__c,Product_Name__c, ListPrice,Domain_Name__c ,Business_Category__c From OpportunityLineItem where opportunityId IN ${oppID} AND TotalPrice!=0`);
@@ -62,19 +71,17 @@ export default async function (event, context, logger) {
                             
         }
     }*/
-    
-        // Commit the Unit of Work with all the previous registered operations
-        const response = await context.org.dataApi.commitUnitOfWork(uow);
-        
-        return response;
-      } catch (err) {
-        const errorMessage = `Failed to insert record. Root Cause : ${err.message}`;
-        logger.error(errorMessage);
-        throw new Error(errorMessage);
-      }
-    //var retResponse = "Deleted revenue record";
 
-    
+    // Commit the Unit of Work with all the previous registered operations
+    const response = await context.org.dataApi.commitUnitOfWork(uow);
 
-    return oliOptyMap;
+    return response;
+  } catch (err) {
+    const errorMessage = `Failed to insert record. Root Cause : ${err.message}`;
+    logger.error(errorMessage);
+    throw new Error(errorMessage);
+  }
+  //var retResponse = "Deleted revenue record";
+
+  return oliOptyMap;
 }
